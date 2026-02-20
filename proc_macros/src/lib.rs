e49ac9ad0f268dcd66c098811f65cf1eae4803dd
Ricardo Cardenes <ricardo.cardenes@noirlab.edu>
2026-02-20 11:00:24 -0300

    Proc macro to derive ValidObject implementations

--- proc_macros/src/lib.rs
+++ proc_macros/src/lib.rs
@@ -1,16 +1,6 @@
 use proc_macro::TokenStream;
 use syn::{parse_macro_input, parse_quote, DeriveInput, ItemImpl};
 
-// Desired result:
-//
-// impl ValidSmalltalkObject for MyStruct {
-//    fn is_valid(obj: &Self) -> bool {
-//        // Check if the object has the correct size. This is valid
-//        // for most special objects
-//        obj.header.is_size(obj::SIZE)
-//    }
-// }
-
 #[proc_macro_derive(ValidSmalltalkObject)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
@@ -22,6 +12,10 @@ pub fn derive(input: TokenStream) -> TokenStream {
                 // Check if the object has the correct size. This is valid
                 // for most special objects
                 obj.header.is_size(Self::SIZE)
+            }
+
+            fn set_invalid(obj: &mut Self) {
+                obj.header.set_invalid();
             }
         }
     );
